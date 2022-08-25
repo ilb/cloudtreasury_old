69eedcb184553d88d0fa8cdef1b9bce6aaa67dba
tyort <neotyo1986@gmail.com>
2022-08-25 20:27:12 +0300

    Выделение вкладки

--- pages/fairprice/header/header.js
+++ pages/fairprice/header/header.js
@@ -2,18 +2,29 @@ import Link from 'next/link';
 import SubMenu from 'antd/lib/menu/SubMenu';
 import { Layout, Menu } from 'antd';
 import { useRouter, withRouter } from 'next/router';
+import { useState } from 'react';
 
 const header = ({}) => {
   const router = useRouter();
+  const [menuItem, setMenuItem] = useState(false);
 
   return (
     <Layout.Header style={{ background: '#fff' }}>
       <Menu theme="light" mode="horizontal" defaultSelectedKeys={['stockValuation']}>
-        <Menu.Item key="stockValuation" onClick={() => router.push('/fairprice/calculate')}>
+        <Menu.Item
+          key="stockValuation"
+          onClick={() => {
+            router.push('/fairprice/calculate');
+            setMenuItem('stockValuation');
+          }}
+          selected={menuItem === 'stockValuation'}>
           Расчёт справедливой стоимости
         </Menu.Item>
         <SubMenu key="exports" title="Отчёты">
-          <Menu.Item key="stockValuationExport">
+          <Menu.Item
+            key="stockValuationExport"
+            onClick={() => setMenuItem('stockValuationExport')}
+            selected={menuItem === 'stockValuationExport'}>
             <Link href="/api/fairprice/export" passHref>
               Определение стоимости ценных бумаг
             </Link>
